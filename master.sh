b48e997b7dc5791c4fc9207b6c68e62151be16f0
haoxingxing <mr.haoxx@gmail.com>
2023-10-29 10:13:30 +0800

    f

--- master.sh
+++ master.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-apt install sshpass
+docker run --network host -it haoxingxing/zjuhpc01 
+
+docker run --network host -it haoxingxing/zjuhpc01 bash
+
 
 ssh-keygen
-
+echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 sshpass -p ZJUMPILAB! ssh-copy-id root@node
+
+echo "192.168.0.22 slots=1
+192.168.0.21 slots=1
+192.168.0.20 slots=1
+192.168.0.19 slots=1
+" > hosts
+export OMPI_ALLOW_RUN_AS_ROOT=1
+export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
+
+mpirun --hostfile hosts uptime

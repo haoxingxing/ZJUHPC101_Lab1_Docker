32e4a8d508ca808334f831b68565fc91093e767b
haoxingxing <mr.haoxx@gmail.com>
2023-10-31 13:41:17 +0800

    c

--- master.sh
+++ master.sh
@@ -8,12 +8,13 @@ ssh-keygen
 echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 sshpass -p ZJUMPILAB! ssh-copy-id root@node
 
-echo "192.168.0.22 slots=1
-192.168.0.21 slots=1
-192.168.0.20 slots=1
-192.168.0.19 slots=1
+echo "192.168.0.7 slots=1
+192.168.0.6 slots=1
+192.168.0.5 slots=1
+192.168.0.4 slots=1
 " > hosts
 export OMPI_ALLOW_RUN_AS_ROOT=1
 export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 
-mpirun --hostfile hosts uptime
+/app/mpi/bin/mpirun --hostfile hosts uptime
+/app/mpi/bin/mpirun --hostfile hosts -wdir /app/hpl-2.3/bin/Linux_HPC/ /app/hpl-2.3/bin/Linux_HPC/xhpl
